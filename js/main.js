c0dfae4cc1b284333843128784a69b73f9a65540
AlfredJKwack <thomas+github@badsoda.com>
2014-11-29 14:27:49 +0100

    Basic Menus - Left and Right

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 
+$(document).ready(function() {
+	$('#left-menu').sidr({
+		name: 'menu-left',
+		side: 'left' // By default
+	});
+	$('#right-menu').sidr({
+		name: 'menu-right',
+		side: 'right'
+	});
+}); 
