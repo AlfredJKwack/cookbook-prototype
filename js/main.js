570a8cdb92948fea68e4b06c89800965dbae88f3
AlfredJKwack <thomas+github@badsoda.com>
2014-11-30 17:12:14 +0100

    Main screen layout
    to fix: pushing of the article header

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 
 $(document).ready(function() {
+	
+	// Create side panels
 	$('#left-menu').sidr({
 		name: 'menu-left',
-		side: 'left' // By default
+		side: 'left'
 	});
 	$('#right-menu').sidr({
 		name: 'menu-right',
 		side: 'right'
 	});
+	
+	// Close all side panels on resize
+	window.onresize = function(){
+		jQuery.sidr('close', 'menu-left');
+		jQuery.sidr('close', 'menu-right');
+	}
 }); 
