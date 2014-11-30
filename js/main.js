b6e4b125d6fcbd9159747277d308764a56fa5d45
AlfredJKwack <thomas+github@badsoda.com>
2014-11-30 11:23:03 +0100

    Revert "Intermediate state"
    This reverts commit a4de2680b34c67aa8129fe129b24d3afa0447309.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,11 @@
 
 $(document).ready(function() {
-	
-	// Create side panels
 	$('#left-menu').sidr({
 		name: 'menu-left',
 		side: 'left' // By default
 	});
 	$('#right-menu').sidr({
 		name: 'menu-right',
-		side: 'right',
-		body: 'body, .main aside'
+		side: 'right'
 	});
-	
-	// Close all side panels on resize
-	window.onresize = function(){
-		jQuery.sidr('close', 'menu-left');
-		jQuery.sidr('close', 'menu-right');
-	}
 }); 
