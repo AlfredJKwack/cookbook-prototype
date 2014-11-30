a4de2680b34c67aa8129fe129b24d3afa0447309
AlfredJKwack <thomas+github@badsoda.com>
2014-11-30 11:22:35 +0100

    Intermediate state
    issue: aside is in fixed position right menu causes some issues.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 
 $(document).ready(function() {
+	
+	// Create side panels
 	$('#left-menu').sidr({
 		name: 'menu-left',
 		side: 'left' // By default
 	});
 	$('#right-menu').sidr({
 		name: 'menu-right',
-		side: 'right'
+		side: 'right',
+		body: 'body, .main aside'
 	});
+	
+	// Close all side panels on resize
+	window.onresize = function(){
+		jQuery.sidr('close', 'menu-left');
+		jQuery.sidr('close', 'menu-right');
+	}
 }); 
