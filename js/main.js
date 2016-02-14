5f0875a8187893f761f348244c855df3851bc74b
AlfredJKwack <lesparents@portamac.home>
2016-02-14 23:16:30 +0100

    Implemented SmartCrop
    Automates the setting of a focus point by analysing the featured image

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
+function calcFocus(cropSize, imgSize) {
+	//converting a topLeft origin with box coordinate axis to a center based coordinate one.
+	var focusPt = {
+		x: (cropSize.x + (cropSize.width/2))-(imgSize.w/2),
+		y: (imgSize.h/2) - (cropSize.y + (cropSize.height/2))		
+	};
+	return focusPt;
+}
 
 $(document).ready(function() {
 	
+	// menu handling
 	$('.left-menu').click(function() {
 		if ($('body').hasClass('is--pushed-left')){$('body').toggleClass('is--pushed-left')}
 		$('body').toggleClass('is--pushed-right');
@@ -11,5 +20,40 @@ $(document).ready(function() {
 		if ($('body').hasClass('is--pushed-right')){$('body').toggleClass('is--pushed-right')}
 		$('body').toggleClass('is--pushed-left');
 	});
-	$('.focuspoint').focusPoint();
+	
+	// crop and size the featured image
+	$('<img />').one('load', function() {
+		
+		//'this' references to the newly created image
+		var imgData = {
+			w: this.width,
+			h: this.height			
+		};
+		
+		// get an appropriate crop window
+		var cropData = SmartCrop.crop(this, {
+				width: 250,		// keeping this slightly smaller than min-height
+				height: 250,
+				minScale: 0.9
+			}, function(result) {
+				return result;
+			}
+		);
+		
+		// get a focus point
+		var focusPoint = calcFocus(cropData.topCrop, imgData);
+		
+		$('div.featured-img.focuspoint').attr({
+			"data-focus-x": focusPoint.x,
+			"data-focus-y": focusPoint.y,
+			"data-focus-w": imgData.w,
+			"data-focus-h": imgData.h
+		});
+		
+		//set the focus point
+		$('.focuspoint').focusPoint();
+		
+	}).attr('src', $('div.featured-img.focuspoint img')[0].src);	
+	
 }); 
+
